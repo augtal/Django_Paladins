d85476947d13445a320efc5944a49dd7079d2b23
augtal <augustas.talacka@gmail.com>
2021-07-29 15:28:54 +0300

    Added initial database model

--- mainApp/models.py
+++ mainApp/models.py
@@ -1,3 +1,30 @@
+from os import name
+from typing import SupportsAbs
 from django.db import models
+from django.db.models.deletion import CASCADE
 
 # Create your models here.
+
+class Role(models.Model):
+    name = models.CharField(max_length=12)
+
+class Champion(models.Model):
+    name = models.CharField(max_length=50)
+    role = models.ForeignKey(Role, on_delete=CASCADE)
+    description = models.CharField(max_length=200)
+
+class ChampionWinRate(models.Model):
+    champion = models.ForeignKey(Champion, on_delete=CASCADE)
+    wins = models.IntegerField(default=0)
+    loses = models.IntegerField(default=0)
+    winRate = models.DecimalField(max_digits=8, decimal_places=5)
+
+class Talent(models.Model):
+    name = models.CharField(max_length=20)
+    type = models.CharField(max_length=20)
+    champion = models.ForeignKey(Champion, on_delete=CASCADE)
+
+class Card(models.Model):
+    name = models.CharField(max_length=20)
+    description = models.CharField(max_length=100)
+    rank = models.IntegerField(default=0)
