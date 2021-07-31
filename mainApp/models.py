d3701d400e3a26282bc2a6d38f50b80d5c7dbac5
augtal <augustas.talacka@gmail.com>
2021-07-31 15:47:36 +0300

    Updated database to better implement API data

--- mainApp/models.py
+++ mainApp/models.py
@@ -1,39 +1,64 @@
 from os import name
 from typing import SupportsAbs
 from django.db import models
+from django.db.models import base
 from django.db.models.deletion import CASCADE
+from django.db.models.expressions import Case
 
 # Create your models here.
 
 class Role(models.Model):
     name = models.CharField(max_length=12)
+    
+class DamageType(models.Model):
+    name = models.CharField(max_length=10)
 
 class Champion(models.Model):
     name = models.CharField(max_length=50)
     role = models.ForeignKey(Role, on_delete=CASCADE)
     description = models.CharField(max_length=200)
+    champion_image = models.URLField()
     
     def __str__(self):
         return self.name
 
-class ChampionWinRate(models.Model):
-    champion = models.ForeignKey(Champion, on_delete=CASCADE)
-    wins = models.IntegerField(default=0)
-    loses = models.IntegerField(default=0)
-    winRate = models.DecimalField(max_digits=8, decimal_places=5)
+class Ability(models.Model):
+    championID = models.ForeignKey(Champion, on_delete=CASCADE)
+    name = models.CharField(max_length=20)
+    description = models.CharField(max_length=50)
+    summary = models.CharField(max_length=50)
+    damageType = models.ForeignKey(DamageType, on_delete=CASCADE)
+    ability_image = models.URLField()
+    
+    def __str__(self):
+        return self.name
 
 class Talent(models.Model):
+    championID = models.ForeignKey(Champion, on_delete=CASCADE)
     name = models.CharField(max_length=20)
     type = models.CharField(max_length=20)
-    champion = models.ForeignKey(Champion, on_delete=CASCADE)
+    description = models.CharField(max_length=20)
+    talent_image = models.URLField()
     
     def __str__(self):
         return self.name
 
 class Card(models.Model):
+    championID = models.ForeignKey(Champion, on_delete=CASCADE)
+    talent = models.CharField(max_length=50)
     name = models.CharField(max_length=20)
     description = models.CharField(max_length=100)
-    rank = models.IntegerField(default=0)
+    type = models.CharField(max_length=20)
+    base = models.IntegerField(default=0)
+    increase = models.IntegerField(default=0)
+    card_image = models.URLField()
     
     def __str__(self):
         return self.name
+
+class ChampionWinRate(models.Model):
+    championID = models.ForeignKey(Champion, on_delete=CASCADE)
+    wins = models.IntegerField(default=0)
+    loses = models.IntegerField(default=0)
+    winRate = models.DecimalField(max_digits=8, decimal_places=5)
+    updated_at = models.DateTimeField()
