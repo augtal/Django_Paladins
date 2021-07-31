9b8946756e6278c561645629922cc2b9ab5325d4
augtal <augustas.talacka@gmail.com>
2021-07-31 15:47:01 +0300

    Added API connection

--- mainApp/views.py
+++ mainApp/views.py
@@ -1,10 +1,31 @@
+import json
 from django.shortcuts import render, redirect
+from pyrez.api import PaladinsAPI
+from pyrez.enumerations.Format import Format
+
+from pathlib import Path
+import os
+import dotenv
+
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+dotenv_file = os.path.join(BASE_DIR, ".env")
+
+if os.path.isfile(dotenv_file):
+    dotenv.load_dotenv(dotenv_file)
 
 # Create your views here.
 
 def index(request):
-    text = "this is main index"
-    
-    return render(request, 'mainApp/index.html', {
-        'text': text
-    })
+    with PaladinsAPI(os.environ['DEV_ID'], os.environ['AUTH_KEY']) as paladins:
+        data = paladins.getChampionCards(2205)
+        
+        # champs = paladins.getGods()
+        
+        text = "this is main index"
+        
+        return render(request, 'mainApp/index.html', {
+            'text': text,
+            'champs': data
+        })
